744736a678a2a6a6d149975a6ac7d98c2aa32c9f
unbanksytv <livethelife.tv@gmail.com>
2023-01-12 14:58:53 +0100

    update OHM

---
+++ pages/covalent.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Data {
+    id: number,
+    name: string,
+    email: string
+}
+
+const MyCovalent: React.FC = () => {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get("https://api.covalenthq.com/v1/1/address/demo.eth/portfolio_v2/?key=ckey_a9971c6f38e540f4aafeb7d498e");
+      setData(result.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="bg-gray-200 p-4">
+      <table className="table-auto">
+        <thead>
+          <tr className="bg-gray-800 text-white">
+            <th className="px-4 py-2">ID</th>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id} className="bg-white text-gray-800">
+              <td className="px-4 py-2">{item.id}</td>
+              <td className="px-4 py-2">{item.name}</td>
+              <td className="px-4 py-2">{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MyCovalent;
